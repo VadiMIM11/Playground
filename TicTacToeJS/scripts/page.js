9a2929c2486ac69b7e3ef4d48f5989b4114f7dac
Vadim <vadimim.99@gmail.com>
2025-01-25 23:32:07 -0500

    TIcTacToe Grid Reworked

--- TicTacToeJS/scripts/page.js
+++ TicTacToeJS/scripts/page.js
@@ -1,13 +1,17 @@
 $(document).ready(function () {
 
+
     let slider = document.getElementById("grid-size-slider");
     let output = document.getElementById("grid-size-label");
     output.innerHTML = slider.value;
   
+    resize_grid(slider.value);
+
     slider.oninput = function () {
       output.innerHTML = this.value;
       resize_grid(this.value);
     }
+
 })
 
 
@@ -16,23 +20,53 @@ function resize_grid(n){
         return;
     }
 
-    let grid = document.getElementById("grid-container");
+    let grid = $("#grid-container");
 
-    let grid_template_string = "";
-    for (let index = 0; index < n; index++) {
-        grid_template_string += "auto ";
-    }
+    // let grid_template_string = "";
+    // for (let index = 0; index < n; index++) {
+    //     grid_template_string += "auto ";
+    // }
 
-    grid.style.gridTemplateColumns = grid_template_string;
+    grid.css("grid-template-columns", `repeat(${n}, 1fr)`);
+    grid.css("grid-template-rows", `repeat(${n}, 1fr)`);
 
-    while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-    }
+    grid.empty();
 
     for (let index = 0; index < n*n; index++) {
-        
-        let cell = document.createElement("div");
-        cell.textContent = index + 1;
-        grid.appendChild(cell);
+        //grid.append(`<div><canvas class="cell-canvas" id=cell_${index}></canvas></div>`);
+        grid.append(`<div class="cell-canvas" id=cell_${index}></div>`);
+        $(`#cell_${index}`).click(cell_canvas_click);
     }
+}
+
+function cell_canvas_click(){
+    console.log(this.id, " was clicked");
+    // let ctx = this.getContext('2d');
+    // let cssWidth = window.getComputedStyle(this).getPropertyValue("width");
+    // let cssHeight = window.getComputedStyle(this).getPropertyValue("height");
+    // this.width = parseFloat(cssWidth);
+    // this.height = parseFloat(cssHeight);
+    // console.log(cssWidth, cssHeight, this.width, this.height);
+    // let x = this.width / 2.0;
+    // let y = this.height / 2.0;
+    // let radius = this.width / 2.0;
+    // let startAngle = 0;
+    // let endAngle = 2 * Math.PI;
+
+    // ctx.beginPath();
+    // ctx.arc(x,y,radius,startAngle,endAngle);
+    // //ctx.fillStyle = 'blue';
+    // //ctx.fill();
+    // ctx.stroke();
+
+    let cssWidth = window.getComputedStyle(this).getPropertyValue("width");
+    let cssHeight = window.getComputedStyle(this).getPropertyValue("height");
+    console.log(cssWidth, cssHeight);
+    let img = document.createElement("img");
+    img.classList.add('cell-canvas');
+    img.setAttribute("src", "src/circle.png");
+    // img.setAttribute("width", cssWidth);
+    // img.setAttribute("height", cssHeight);
+    this.appendChild(img);
+
 }
