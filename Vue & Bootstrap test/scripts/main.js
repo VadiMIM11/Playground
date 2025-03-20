c86556a9591d01e0bf06bdcbf51fee58e59532ad
Vadim <vadimim.99@gmail.com>
2025-03-20 18:04:13 -0400

    Vue & Bootstrap Theme Switch

--- Vue & Bootstrap test/scripts/main.js
+++ Vue & Bootstrap test/scripts/main.js
@@ -1,27 +1,28 @@
+
 const { createApp } = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
             theme: 'dark',
+            direction: 'row',
             // count: 0,
         }
     },
 
-    methods:{
-        toggleTheme:function(){
+    methods: {
+        toggleTheme: function () {
             console.log("Theme Toggled");
             if (this.theme == 'light') {
                 console.log("Light Theme!");
                 this.theme = 'dark';
             }
-            else
-            {
+            else {
                 console.log("Dark Theme!");
                 this.theme = 'light';
             }
             this.count++;
-            
+
             console.log(this.theme);
         }
     }
